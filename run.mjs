e519608ea61254e4c03860f4706ae76522cd6d36
Tyler Brazier <tyler@tylerbrazier.com>
2023-09-03 19:43:38 -0600

    Allow restricting runs to same suit or color

--- run.mjs
+++ run.mjs
@@ -1,14 +1,15 @@
-// TODO check for probability of all same suit
-
-export { containsRun, }
+export { containsRun, suitCriteria }
 import { simulate } from './base.mjs'
 import { fileURLToPath } from 'node:url'
 
 // Returns the first run of size n found, false otherwise.
-// If aces are high and low, then wrapping is allowed (K,A,2 counts as a run)
-function containsRun(hand, n, acesLow = true, acesHigh = true) {
+// If aces are high and low, then wrapping is allowed (K,A,2 counts as a run).
+function containsRun(hand, n, acesLow=true, acesHigh=false,
+		sameSuit=false, sameColor=false) {
 	for (var i = 0; i < hand.length; i++) {
-		var sequence = run(hand, n, [hand[i]], acesLow, acesHigh)
+		var possibleSuits = suitCriteria(hand[i], sameSuit, sameColor)
+		var sequence = run(hand, n, [hand[i]], acesLow, acesHigh,
+			possibleSuits)
 		if (sequence) return sequence
 	}
 	return false
@@ -16,22 +17,22 @@ function containsRun(hand, n, acesLow = true, acesHigh = true) {
 
 // Recursive function that builds up a sequence of cards from hand,
 // returning the sequence if it's n size or false if it can't reach it.
-function run(hand, n, sequence, acesLow, acesHigh) {
+function run(hand, n, sequence, acesLow, acesHigh, possibleSuits) {
 	if (sequence.length >= n) return sequence
 	var nextRank = nextRankInSequence(sequence.at(-1), acesLow, acesHigh)
-	var nextCard = containsRank(hand, nextRank)
+	var nextCard = containsCard(hand, nextRank, possibleSuits)
 	if (nextCard) {
 		sequence.push(nextCard)
-		return run(hand, n, sequence, acesLow, acesHigh)
+		return run(hand, n, sequence, acesLow, acesHigh, possibleSuits)
 	} else {
-		false
+		return false
 	}
 }
 
-function containsRank(hand, rank) {
+function containsCard(hand, rank, possibleSuits) {
 	if (!rank) return false
 	for (var card of hand)
-		if (card.rank === rank)
+		if (card.rank === rank && possibleSuits.includes(card.suit))
 			return card
 	return false
 }
@@ -48,14 +49,34 @@ function nextRankInSequence(card, acesLow, acesHigh) {
 	}
 }
 
+// Returns an array of suits given a card and suit/color criteria
+function suitCriteria(card, sameSuit, sameColor) {
+	if (sameSuit) return [card.suit]
+	if (sameColor) {
+		var blackSuits = ['c', 's']
+		var redSuits = ['d', 'h']
+		if (blackSuits.includes(card.suit)) return blackSuits
+		if (redSuits.includes(card.suit)) return redSuits
+	}
+	return ['c', 'd', 'h', 's']
+}
+
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
-	for (var h = 10; h <= 20; h+=5) {
+	for (var h = 5; h <= 5; h+=1) {
 		console.log(h, 'card hand:')
-		for (var r = 3; r <= 10; r++) {
+		for (var r = 2; r <= 3; r++) {
 			if (r > h) continue;
 
-			process.stdout.write('Run of '+r+' (Aces low): ')
-			simulate(hand => containsRun(hand, r, true, false), h)
+			process.stdout.write('Run of '+r+' same suit: ')
+			simulate(hand => containsRun(hand, r, true, false,
+				true, false), h)
+
+			process.stdout.write('Run of '+r+' same color: ')
+			simulate(hand => containsRun(hand, r, true, false,
+				false, true), h)
+
+			// process.stdout.write('Run of '+r+' (Aces low): ')
+			// simulate(hand => containsRun(hand, r, true, false), h)
 
 			// process.stdout.write('Aces high only: ')
 			// simulate(hand => containsRun(hand, r, false, true), h)
--- run_test.mjs
+++ run_test.mjs
@@ -1,6 +1,6 @@
 import test from 'node:test'
 import assert from 'node:assert'
-import { containsRun } from './run.mjs'
+import { containsRun, suitCriteria } from './run.mjs'
 
 test('containsRun', async t => {
 	await t.test('no aces run of 3', t => {
@@ -96,5 +96,169 @@ test('containsRun', async t => {
 		]
 
 		assert.deepStrictEqual(containsRun(hand, 3), expected)
+	})
+
+	await t.test('false when run is not same color', t => {
+		var hand = [
+			{ rank:'4', suit:'s' },
+			{ rank:3, suit:'h' },
+			{ rank:'K', suit:'d' },
+			{ rank:'Q', suit:'s' },
+			{ rank:2, suit:'c' },
+		]
+		var acesHigh = false
+		var acesLow = true
+		var sameSuit = false
+		var sameColor = true
+		var expected = false
+		assert.deepStrictEqual(containsRun(hand, 3, acesLow, acesHigh,
+			sameSuit, sameColor), expected)
+	})
+	await t.test('false when run is not same suit (tho colors match)', t => {
+		var hand = [
+			{ rank:'4', suit:'s' },
+			{ rank:3, suit:'c' },
+			{ rank:'K', suit:'d' },
+			{ rank:'Q', suit:'s' },
+			{ rank:2, suit:'c' },
+		]
+		var acesHigh = false
+		var acesLow = true
+		var sameSuit = true
+		var sameColor = false
+		var expected = false
+		assert.deepStrictEqual(containsRun(hand, 3, acesLow, acesHigh,
+			sameSuit, sameColor), expected)
+	})
+	await t.test('false when run is not same suit (and colors do not match)', t => {
+		var hand = [
+			{ rank:'4', suit:'s' },
+			{ rank:3, suit:'d' },
+			{ rank:'K', suit:'d' },
+			{ rank:'Q', suit:'s' },
+			{ rank:2, suit:'c' },
+		]
+		var acesHigh = false
+		var acesLow = true
+		var sameSuit = true
+		var sameColor = false
+		var expected = false
+		assert.deepStrictEqual(containsRun(hand, 3, acesLow, acesHigh,
+			sameSuit, sameColor), expected)
+	})
+	await t.test('false when sameColor=true && sameSuit=true and colors match but suit does not', t => {
+		var hand = [
+			{ rank:'4', suit:'d' },
+			{ rank:3, suit:'h' },
+			{ rank:'K', suit:'d' },
+			{ rank:'Q', suit:'s' },
+			{ rank:2, suit:'d' },
+		]
+		var acesHigh = false
+		var acesLow = true
+		var sameSuit = true
+		var sameColor = true
+		var expected = false
+		assert.deepStrictEqual(containsRun(hand, 3, acesLow, acesHigh,
+			sameSuit, sameColor), expected)
+	})
+	await t.test('true when is same color (and suit)', t => {
+		var hand = [
+			{ rank:'4', suit:'d' },
+			{ rank:3, suit:'d' },
+			{ rank:'K', suit:'d' },
+			{ rank:'Q', suit:'s' },
+			{ rank:2, suit:'d' },
+		]
+		var acesHigh = false
+		var acesLow = false
+		var sameSuit = false
+		var sameColor = true
+		var expected = false
+		assert.deepStrictEqual(containsRun(hand, 3, acesLow, acesHigh,
+			sameSuit, sameColor), expected)
+	})
+	await t.test('true when is same color (but not suit)', t => {
+		var hand = [
+			{ rank:'4', suit:'h' },
+			{ rank:3, suit:'d' },
+			{ rank:'K', suit:'d' },
+			{ rank:'Q', suit:'s' },
+			{ rank:2, suit:'h' },
+		]
+		var acesHigh = false
+		var acesLow = false
+		var sameSuit = false
+		var sameColor = true
+		var expected = false
+		assert.deepStrictEqual(containsRun(hand, 3, acesLow, acesHigh,
+			sameSuit, sameColor), expected)
+	})
+	await t.test('true when is same suit', t => {
+		var hand = [
+			{ rank:'4', suit:'h' },
+			{ rank:3, suit:'h' },
+			{ rank:'K', suit:'d' },
+			{ rank:'Q', suit:'s' },
+			{ rank:2, suit:'h' },
+		]
+		var acesHigh = false
+		var acesLow = false
+		var sameSuit = true
+		var sameColor = false
+		var expected = false
+		assert.deepStrictEqual(containsRun(hand, 3, acesLow, acesHigh,
+			sameSuit, sameColor), expected)
+	})
+})
+test('suitCriteria', async t => {
+	await t.test('d sameSuit -> [d]', t => {
+		var card = { rank: 'A', suit: 'd' }
+		var sameSuit = true
+		var sameColor = false
+		var expected = ['d']
+		assert.deepStrictEqual(suitCriteria(card, sameSuit, sameColor), expected)
+	})
+	await t.test('d sameColor -> [d,h]', t => {
+		var card = { rank: '4', suit: 'd' }
+		var sameSuit = false
+		var sameColor = true
+		var expected = ['d', 'h']
+		assert.deepStrictEqual(suitCriteria(card, sameSuit, sameColor), expected)
+	})
+	await t.test('c sameSuit -> [c]', t => {
+		var card = { rank: '7', suit: 'c' }
+		var sameSuit = true
+		var sameColor = false
+		var expected = ['c']
+		assert.deepStrictEqual(suitCriteria(card, sameSuit, sameColor), expected)
+	})
+	await t.test('s sameColor -> [c,s]', t => {
+		var card = { rank: 'A', suit: 's' }
+		var sameSuit = false
+		var sameColor = true
+		var expected = ['c', 's']
+		assert.deepStrictEqual(suitCriteria(card, sameSuit, sameColor), expected)
+	})
+	await t.test('s sameColor && sameSuit -> [s]', t => {
+		var card = { rank: 'Q', suit: 's' }
+		var sameSuit = true
+		var sameColor = true
+		var expected = ['s']
+		assert.deepStrictEqual(suitCriteria(card, sameSuit, sameColor), expected)
+	})
+	await t.test('h sameColor && sameSuit -> [h]', t => {
+		var card = { rank: 'K', suit: 'h' }
+		var sameSuit = true
+		var sameColor = true
+		var expected = ['h']
+		assert.deepStrictEqual(suitCriteria(card, sameSuit, sameColor), expected)
+	})
+	await t.test('sameColor=false && sameSuit=false -> [c, d, h, s]', t => {
+		var card = { rank: 'K', suit: 'h' }
+		var sameSuit = false
+		var sameColor = false
+		var expected = ['c', 'd', 'h', 's']
+		assert.deepStrictEqual(suitCriteria(card, sameSuit, sameColor), expected)
 	})
 })
